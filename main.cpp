e00c0d55f39ce0ebdae4911c3a1f9c71336c15c9
zachary <>
2024-06-27 07:54:40 -0500

    Added GLFW and glad

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    if (!glfwInit()) {
+        std::cout << "failed to initialize GLFW" << std::endl;
+        return -1; 
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello, window!", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "failed to initialize GLFW window" << std::endl;
+        return -1; 
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 }
